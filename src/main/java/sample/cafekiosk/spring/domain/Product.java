fbc6e8e066615ee0278eaaad4346970f7e7195c6
sungsanghyun <sos9210@naver.com>
2024-01-22 23:03:33 +0900

    persistence layer테스트

---
+++ src/main/java/sample/cafekiosk/spring/domain/Product.java
@@ -0,0 +1,42 @@
+package sample.cafekiosk.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@EnableJpaAuditing
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingStatus sellingStatus;
+
+    private String name;
+
+    private int price;
+
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+}
--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.unit;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -19,6 +20,10 @@ public class CafeKioskTest {
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverageList().get(0).getName());
     }
 
+    //DisplayName을 작성성할때 떤 문구가 더 이해하기 쉬울지 고민해보자.
+    //테스트 행위를 함으로써 어떤 결과가 나오는지 담는것이 더 좋아보인다. ex) 음료 1개 추가하면 주문 목록에 담긴다.
+    //@DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -74,8 +79,10 @@ public class CafeKioskTest {
         Assertions.assertThat(cafeKiosk.getBeverageList()).isEmpty();
     }
 
+    @DisplayName("주문목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        //given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -83,8 +90,10 @@ public class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        //when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        //then
         Assertions.assertThat(totalPrice).isEqualTo(8500);
     }
     @Test
