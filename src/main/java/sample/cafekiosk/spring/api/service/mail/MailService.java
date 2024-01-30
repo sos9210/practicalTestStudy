51d84b68f42e7989c038aa73942c204285cd6e1a
sungsanghyun <sos9210@naver.com>
2024-01-30 23:21:37 +0900

    @Mock, @Spy, @InjectMocks 사용법

--- src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
@@ -22,6 +22,9 @@ public class MailService {
                             .content(content)
                             .build()
                     );
+            mailSendClient.a();
+            mailSendClient.b();
+            mailSendClient.c();
             return true;
         }
         return false;
---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,70 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    //@Mock을 사용한 주입은 @ExtendWith(MockitoExtension.class)를 사용해줘야 한다.
+    //@Mock
+    @Spy
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    //@InjectMocks을 사용한 객체의 생성자 파라미터에 @Mock을 사용한 객체들을 이용해서 생성한다.
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        //given
+
+        //위 필드주입방식의 @Mock과 같다
+//        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
+//        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
+
+ //       MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+//        Mockito.when(mailSendClient.sendEmail(
+//                        ArgumentMatchers.any(String.class),
+//                        ArgumentMatchers.any(String.class),
+//                        ArgumentMatchers.any(String.class),
+//                        ArgumentMatchers.any(String.class)
+//                )
+//        ).thenReturn(true);
+
+        //@Spy를 사용하면 원하는 부분만 스터빙하고싶은 아래처럼 사용한다
+        Mockito.doReturn(true)
+                .when(mailSendClient)
+                .sendEmail(
+                        ArgumentMatchers.any(String.class),
+                        ArgumentMatchers.any(String.class),
+                        ArgumentMatchers.any(String.class),
+                        ArgumentMatchers.any(String.class)
+                );
+
+
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        Assertions.assertThat(result).isTrue();
+        Mockito.verify(mailSendHistoryRepository, Mockito.times(1))
+                .save(Mockito.any(MailSendHistory.class));
+    }
+
+}
