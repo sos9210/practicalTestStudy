b23369c632621bddc54a388aef16b40475281c8c
sungsanghyun <sos9210@naver.com>
2024-02-12 22:29:19 +0900

    라이브러리 학습용 테스트작성 예시

---
+++ src/test/java/sample/cafekiosk/learning/GuavaLearningTest.java
@@ -0,0 +1,101 @@
+package sample.cafekiosk.learning;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+public class GuavaLearningTest {
+
+    @DisplayName("주어진 개수만큼 List를 파티셔닝 한다.")
+    @Test
+    void partitionLearningTest1() {
+        //given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+        //when
+        List<List<Integer>> partition = Lists.partition(integers, 3);
+
+        //then
+        Assertions.assertThat(partition).hasSize(2)
+                .isEqualTo(List.of(
+                        List.of(1,2,3),List.of(4,5,6)
+                ));
+    }
+    @DisplayName("주어진 개수만큼 List를 파티셔닝 한다.")
+    @Test
+    void partitionLearningTest2() {
+        //given
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+
+        //when
+        List<List<Integer>> partition = Lists.partition(integers, 4);
+
+        //then
+        Assertions.assertThat(partition).hasSize(2)
+                .isEqualTo(List.of(
+                        List.of(1,2,3,4),List.of(5,6)
+                ));
+    }
+    @DisplayName("멀티맵 기능은 하나의 key에 여러개의 value를 저장한다")
+    @Test
+    void multiMapLearningTest1() {
+        //하나의 key에 여러개의 value
+        //given
+        Multimap<String,String> multimap = ArrayListMultimap.create();
+        multimap.put("커피","아메리카노");
+        multimap.put("커피","카페라떼");
+        multimap.put("커피","카푸치노");
+        multimap.put("베이커리","크루아상");
+        multimap.put("베이커리","식빵");
+
+        //when
+        Collection<String> strings = multimap.get("커피");
+
+        //then
+        Assertions.assertThat(strings).hasSize(3)
+                .isEqualTo(List.of("아메리카노","카페라떼","카푸치노"));
+    }
+    
+    @DisplayName("멀티맵 기능은 하나의 key에 여러개의 value를 저장한다")
+    @TestFactory
+    Collection<DynamicTest> multiMapLearningTest2() {
+        //하나의 key에 여러개의 value
+        //given
+        Multimap<String,String> multimap = ArrayListMultimap.create();
+        multimap.put("커피","아메리카노");
+        multimap.put("커피","카페라떼");
+        multimap.put("커피","카푸치노");
+        multimap.put("베이커리","크루아상");
+        multimap.put("베이커리","식빵");
+
+        return List.of(
+                DynamicTest.dynamicTest("1개 value 삭제", () -> {
+                    //when
+                    multimap.remove("커피","카푸치노");
+
+
+                    //then
+                    Collection<String> results = multimap.get("커피");
+                    Assertions.assertThat(results).hasSize(2)
+                            .isEqualTo(List.of("아메리카노","카페라떼"));
+                }),
+                DynamicTest.dynamicTest("1개 key 삭제", () -> {
+                    //when
+                    multimap.removeAll("커피");
+
+
+                    //then
+                    Collection<String> results = multimap.get("커피");
+                    Assertions.assertThat(results).isEmpty();
+                })
+        );
+    }
+}
